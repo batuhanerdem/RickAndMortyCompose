1b686b74f930da618fe85e37cd6415fc86825ae3
Batuhan Erdem <79407999+batuhanerdem@users.noreply.github.com>
2024-08-05 21:47:33 +0300

    some import removed

--- app/src/main/java/com/example/rickandmortycompose/ui/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortycompose/ui/MainActivity.kt
@@ -10,10 +10,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
