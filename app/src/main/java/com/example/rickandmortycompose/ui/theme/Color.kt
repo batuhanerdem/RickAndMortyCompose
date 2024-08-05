3be69898ac1b79a276addf6880aacf443b437c94
Batuhan Erdem <79407999+batuhanerdem@users.noreply.github.com>
2024-08-05 08:54:22 +0300

    added new color and drawables

--- app/src/main/java/com/example/rickandmortycompose/ui/theme/Color.kt
+++ app/src/main/java/com/example/rickandmortycompose/ui/theme/Color.kt
@@ -11,4 +11,5 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val PortalGreen = Color(0xFF2EB561)
+val LightPortalGreen = Color(0XFFC2F0D4)
 val Background = Color(0XFFF0FAF2)
